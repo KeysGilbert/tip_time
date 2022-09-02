12b3a5b8ed0206f8f619033c86f91ca41862c172
KeysGilbert <gbk5070@psu.edu>
2022-09-02 15:04:32 -0400

    calculate tip

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener() {
+            calculateTip()
+        }
+    }
+
+    fun calculateTip() {
+        val textfieldValue = binding.costOfService.text.toString()
+        val cost = textfieldValue.toDouble()
+        val selectedPercentage = binding.tipOptions.checkedRadioButtonId
+
+        val tip = when(selectedPercentage) {
+            R.id.twenty_percent -> 0.20
+            R.id.eightteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var result = cost * tip
+        val roundUp = binding.roundSwitch.isChecked
+
+        if(roundUp) {
+            result = ceil(result)
+        }
     }
 }
